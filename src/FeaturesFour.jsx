7049cb8ea520cf342910daaf0f74c5a6a434a7b8
DOriginalSamxie <samadrufai@gmail.com>
2022-12-04 23:58:43 +0100

    navbar completed

--- src/FeaturesFour.jsx
+++ src/FeaturesFour.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
 import Location from '../src/Assets/Location.svg'
+import Calling from '../src/Assets/Calling.svg'
+import Message from '../src/Assets/Message.svg'
+import Facebook from '../src/Assets/facebook.svg'
+import Twitter from '../src/Assets/twitter.svg'
+import Linkedin from '../src/Assets/linkedin.svg'
+import Instagram from '../src/Assets/instagram.svg'
 
 const FeaturesFour = () => {
   return (
-    <section className='featurestwo md:mt-[89.33px]  mt-[20px] bg-[#F4FBFF] px-12 md:px-2 lg:px-0'>
+    <section className='featurestwo md:mt-[89.33px]  mt-[20px] bg-[#F4FBFF] px-12 md:px-2 lg:px-0 pb-[140px]'>
       <div>
-        <form className=' container mx-auto max-w-[1539px] flex flex-col lg:flex-row xl:pl-[272px] space-x-[98px] lg:px-4'>
+        <form className=' container mx-auto max-w-[1539px]  flex flex-col lg:flex-row xl:pl-[272px] lg:space-x-[98px] lg:px-4'>
           <div className='max-w-[575px]'>
             <h1 className='max-w-[450px] md:pt-[64px] text-[#4B4C4E] lg:leading-[47px] lg:text-[36px] font-bold '>
               <span className='border-b-[5px] border-b-[#4EB9E3]'>Build</span>{' '}
@@ -83,13 +89,40 @@ const FeaturesFour = () => {
               </div>
             </div>
           </div>
-          <div className=' w-1/2'>
-            <h2 className='lg:pt-[293px]'>Contact us</h2>
+          <div className=' '>
+            <h2 className='lg:pt-[293px] pb-[17px]'>Contact us</h2>
             <div className='  '>
-              <p className='flex items-center gap-[20px]'>
-                <img src={Location} />
+              <p className='flex items-center gap-[20px] text-[16px] leading-[26px] pb-[25px]'>
+                <img src={Location} alt='location' />
                 4517 Washington Ave. Manchester, Kentucky 39495
               </p>
+            </div>
+            <div className='  '>
+              <ul className='flex gap-[20px] pb-[25px]'>
+                <div>
+                  <img src={Calling} alt='calling' />
+                </div>
+                <div>
+                  <li>(505) 555-0119</li>
+                  <li>(505) 555-0120</li>
+                  <li>(505) 555-0125</li>
+                </div>
+              </ul>
+            </div>
+            <div>
+              <p className='flex gap-[20px] pb-[51px]'>
+                <img src={Message} alt='message' />
+                consult@vacanxe.com
+              </p>
+            </div>
+            <div>
+              <h2 className='pb-[15px]'>Follow us:</h2>
+              <div className='flex gap-[20px]'>
+                <img src={Facebook} alt='facebook' />
+                <img src={Twitter} alt='twiiter' />
+                <img src={Linkedin} alt='linkedin' />
+                <img src={Instagram} alt='instagram' />
+              </div>
             </div>
           </div>
         </form>
